887f375438b8c870a655ee9fbafd9123e0d48197
Chachan23 <162387874+Chachan23@users.noreply.github.com>
2024-06-06 09:37:21 -0700

    Add files via upload

---
+++ preguntas.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "¿Cual es el material que se utiliza para la puerta?",
+    answer: "Madera",
+    options: [
+      "Madera",
+      "Metal",
+      "Cascaron de huevo",
+      "zink"
+    ]
+  },
+    {
+    numb: 2,
+    question: ". ¿Que materiales se utilizaron para las paredes?",
+    answer: "Madera",
+    options: [
+      "Plastica",
+      "Carton",
+      "Madera",
+      "Metal"
+    ]
+  },
+    {
+    numb: 3,
+    question: "¿Que materiales se utilizaon para las paredes?",
+    answer: "Carton",
+    options: [
+      "Carton",
+      "Cascaron de Huevo",
+      "Madera",
+      "Plastiina"
+    ]
+  },
+    {
+    numb: 4,
+    question: "¿De cuantos pisos es la casa?",
+    answer: "1",
+    options: [
+      "4",
+      "3",
+      "1",
+      "2"
+    ]
+  },
+    {
+    numb: 5,
+    question: "¿De cuantos cuartos es la casa?",
+    answer: "3",
+    options: [
+      "2",
+      "3",
+      "7",
+      "No se"
+    ]
+  },
+  ];
+  
